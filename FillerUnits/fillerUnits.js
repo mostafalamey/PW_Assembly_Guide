bd97cf762428dea357ef3a639aaf112717f9014f
Mostafa Lamey <mlamey@outlook.com>
2024-04-23 03:10:54 +0200

    Filler Units

---
+++ FillerUnits/fillerUnits.js
@@ -0,0 +1,63 @@
+
+const logoBtn = document.querySelector('#logo');
+
+const flbs8 = document.querySelector('#flbs8');
+const flbs12 = document.querySelector('#flbs12');
+
+const flwl398 = document.querySelector('#flwl398');
+const flwl3912 = document.querySelector('#flwl3912');
+
+const flwl788 = document.querySelector('#flwl788');
+const flwl7812 = document.querySelector('#flwl7812');
+
+const flhi8 = document.querySelector('#flhi8');
+const flhi12 = document.querySelector('#flhi12');
+
+const fltl8 = document.querySelector('#fltl8');
+const fltl12 = document.querySelector('#fltl12');
+
+
+
+logoBtn.addEventListener('click', () => {
+    window.location.href = '../index.html';
+});
+
+
+flbs8.addEventListener('click', () => {
+    window.location.href = 'Filler_Base/fillerbase.html';
+});
+flbs12.addEventListener('click', () => {
+    window.location.href = 'Filler_Base/fillerbase.html';
+});
+
+
+flwl398.addEventListener('click', () => {
+    window.location.href = 'Filler_Wall_39/fillerwall_39.html';
+});
+flwl3912.addEventListener('click', () => {
+    window.location.href = 'Filler_Wall_39/fillerwall_39.html';
+});
+
+
+flwl788.addEventListener('click', () => {
+    window.location.href = 'Filler_Wall_78/fillerwall_78.html';
+});
+flwl7812.addEventListener('click', () => {
+    window.location.href = 'Filler_Wall_78/fillerwall_78.html';
+});
+
+
+flhi8.addEventListener('click', () => {
+    window.location.href = 'Filler_Heigh/fillerheigh.html';
+});
+flhi12.addEventListener('click', () => {
+    window.location.href = 'Filler_Heigh/fillerheigh.html';
+});
+
+
+fltl8.addEventListener('click', () => {
+    window.location.href = 'Filler_Tall/fillertall.html';
+});
+fltl12.addEventListener('click', () => {
+    window.location.href = 'Filler_Tall/fillertall.html';
+});
