64aaae9b16ba8f35a84d4cf01c614cc60e2c6de3
Mostafa Lamey <mlamey@outlook.com>
2024-06-21 08:16:44 +0300

    TallUnit2Doors1DrawerMicrowave

--- TallUnits/TallUnit_1Door1Drawer_Microwave/videoController.js
+++ TallUnits/TallUnit_1Door1Drawer_Microwave/videoController.js
@@ -12,7 +12,8 @@ const backBtn = document.querySelector('#back');
 const nextBtn = document.querySelector('#next');
 
 // Array of bookmarks
-const bookmarks = [0, 21.75, 53.1, 86.1, 112.75, 136.6, 155.4, 182.7, 253.9, 279.5, 303, 327];
+//                 0,  1,     2,     3,    4,     5       6     7      8       9     10     11     12    13     14     15      16     17   18     19     20
+const bookmarks = [0, 21.75, 53.1, 86.1, 112.75, 136.6, 155.4, 182.7, 253.9, 279.8, 303.2, 321.3, 355.3, 391.4, 416.7, 438.3, 465.8, 496, 519.3, 537.5, 551.7, 569];
 // Current bookmark index
 let currentBookmarkIndex = 0;
 
